33a3ec414769b319f0a9079bc72bed4bad787c1b
Nicolas <nico_Cartellone@hotmail.com>
2021-07-10 02:07:22 -0300

    first commint

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+//La URL base de la api 
+axios.defaults.baseURL = 'http://localhost:3000/api';
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
